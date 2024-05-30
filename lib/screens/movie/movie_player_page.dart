efe41b28e0050f5bcf8afb713e03dad5235c8662
Foster Amponsah Asante <fostersnt@gmail.com>
2024-05-30 13:51:50 +0000

    Added thumbnail for videos

--- lib/screens/movie/movie_player_page.dart
+++ lib/screens/movie/movie_player_page.dart
@@ -1,3 +1,5 @@
+// import 'package:appinio_video_player/appinio_video_player.dart';
+import 'package:appinio_video_player/appinio_video_player.dart';
 import 'package:flutter/material.dart';
 
 class MoviePlayerPage extends StatefulWidget {
@@ -8,8 +10,33 @@ class MoviePlayerPage extends StatefulWidget {
 }
 
 class _MoviePlayerPageState extends State<MoviePlayerPage> {
+  late CustomVideoPlayerController _customVideoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializaVideoPlayer();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomVideoPlayer(
+          customVideoPlayerController: _customVideoPlayerController),
+    );
+  }
+
+  void initializaVideoPlayer() {
+    CachedVideoPlayerController _cachedVideoPlayerController;
+    _cachedVideoPlayerController =
+        CachedVideoPlayerController.asset('lib/assets/videos/crabs.mp4')
+          ..initialize().then((value) => setState(() {}));
+    _customVideoPlayerController = CustomVideoPlayerController(
+      context: context,
+      videoPlayerController: _cachedVideoPlayerController,
+      customVideoPlayerSettings: CustomVideoPlayerSettings(
+        thumbnailWidget: Image.asset('lib/assets/images/banner.png'),
+      ),
+    );
   }
 }
