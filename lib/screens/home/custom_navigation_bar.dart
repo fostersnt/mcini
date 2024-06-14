cd20bf3000c8f188c1b78f14ab9543721f6e8d26
Foster Amponsah Asante <fostersnt@gmail.com>
2024-06-14 15:30:38 +0000

    Added a custom bottom navigation bar

--- lib/screens/home/custom_navigation_bar.dart
+++ lib/screens/home/custom_navigation_bar.dart
@@ -11,7 +11,6 @@ class CustomNavigationBar extends StatefulWidget {
 
 class _CustomNavigationBarState extends State<CustomNavigationBar> {
   int selectedIndex = 0;
-  // double myPadding = 10;
 
   final List<Widget> _pages = [
     const HomePage(),
@@ -24,6 +23,7 @@ class _CustomNavigationBarState extends State<CustomNavigationBar> {
   @override
   Widget build(BuildContext context) {
     final Size deviceSize = MediaQuery.of(context).size;
+    double iconSize = deviceSize.width * 0.08;
     final List<BottomNavigationBarItem> _bottomNavBarItems = [
       BottomNavigationBarItem(
         icon: Icon(
@@ -54,18 +54,92 @@ class _CustomNavigationBarState extends State<CustomNavigationBar> {
     return Scaffold(
       body: _pages[selectedIndex],
       bottomNavigationBar: Container(
+        color: AppColors.bottomNavColor,
         height: 70,
-        child: BottomNavigationBar(
-          iconSize: deviceSize.width * 0.08,
-          elevation: 1,
-          backgroundColor: AppColors.bottomNavColor,
-          onTap: (value) {
-            setState(() {
-              selectedIndex = value;
-            });
-            print('CURRENT INDEX: $value');
-          },
-          items: _bottomNavBarItems,
+        // child: BottomNavigationBar(
+        //   iconSize: deviceSize.width * 0.08,
+        //   elevation: 1,
+        //   backgroundColor: AppColors.bottomNavColor,
+        //   onTap: (value) {
+        //     setState(() {
+        //       selectedIndex = value;
+        //     });
+        //     print('CURRENT INDEX: $value');
+        //   },
+        //   items: _bottomNavBarItems,
+        // ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            //Home icon
+            InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 0;
+                });
+              },
+              child: Container(
+                child: Icon(
+                  Icons.home_filled,
+                  color: selectedIndex == 0
+                      ? AppColors.blueColor
+                      : AppColors.whiteColor,
+                  size: iconSize,
+                ),
+              ),
+            ),
+            //Favorite icon
+            InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 1;
+                });
+              },
+              child: Container(
+                child: Icon(
+                  Icons.favorite,
+                  color: selectedIndex == 1
+                      ? AppColors.blueColor
+                      : AppColors.whiteColor,
+                  size: iconSize,
+                ),
+              ),
+            ),
+            //Search icon
+            InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 2;
+                });
+              },
+              child: Container(
+                child: Icon(
+                  Icons.search_outlined,
+                  color: selectedIndex == 2
+                      ? AppColors.blueColor
+                      : AppColors.whiteColor,
+                  size: iconSize,
+                ),
+              ),
+            ),
+            //Profile icon
+            InkWell(
+              onTap: () {
+                setState(() {
+                  selectedIndex = 3;
+                });
+              },
+              child: Container(
+                child: Icon(
+                  Icons.person,
+                  color: selectedIndex == 3
+                      ? AppColors.blueColor
+                      : AppColors.whiteColor,
+                  size: iconSize,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
