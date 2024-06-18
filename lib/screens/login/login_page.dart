406bc68da63b68013fd4290f8dc1751ba04c538d
Foster Amponsah Asante <fostersnt@gmail.com>
2024-06-18 10:35:49 +0000

    Update

--- lib/screens/login/login_page.dart
+++ lib/screens/login/login_page.dart
@@ -9,86 +9,102 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size deviceSize = MediaQuery.of(context).size;
-    return Scaffold(
-      // appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: AppColors.blueColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: deviceSize.width * 0.08,
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(0),
+          child: Stack(
+            children: [
+              Container(
+                width: deviceSize.width,
+                height: deviceSize.height,
+                child: Image.asset(
+                  'lib/assets/images/banner.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              child: TextField(
-                controller: phoneNumberController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  fillColor: AppColors.txtFieldBgColor,
-                  filled: true,
-                  // labelText: 'Phone Number',
-                  hintText: 'Enter your phone number',
-                  hintStyle: TextStyle(color: AppColors.whiteColor),
-                  border: OutlineInputBorder(
-                    // borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      color: AppColors.blueColor,
-                      width: 2,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: AppColors.blueColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: deviceSize.width * 0.08,
+                        ),
+                      ),
                     ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors.blueColor,
-                      width: 1,
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: AppColors.blueColor,
-                      width: 2,
+                    Container(
+                      child: TextField(
+                        controller: phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          fillColor: AppColors.txtFieldBgColor,
+                          filled: true,
+                          // labelText: 'Phone Number',
+                          hintText: 'Enter phone number',
+                          hintStyle: TextStyle(color: AppColors.whiteColor),
+                          border: OutlineInputBorder(
+                            // borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.blueColor,
+                              width: 2,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.blueColor,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.blueColor,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: deviceSize.width,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            AppColors.blueColor, // Button background color
+                          ),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  5), // No rounded corners
+                            ),
+                          ),
+                        ),
+                        onPressed: () => {},
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(
+                            color: AppColors.whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: deviceSize.width * 0.05,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: deviceSize.width,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    AppColors.blueColor, // Button background color
-                  ),
-                  shape: MaterialStateProperty.all<OutlinedBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(5), // No rounded corners
-                    ),
-                  ),
-                ),
-                onPressed: () => {},
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                    color: AppColors.whiteColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: deviceSize.width * 0.05,
-                  ),
-                ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
