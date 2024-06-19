a27c3bf33a061b88380e0aebb92605f3ef6bcc92
Foster Amponsah Asante <fostersnt@gmail.com>
2024-06-19 13:19:00 +0000

    Added icons to the snackbar display

--- lib/utilities/app_colors.dart
+++ lib/utilities/app_colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AppColors {
   //This class is purposely for defining the colors that would be used throughout the application
@@ -13,19 +14,43 @@ class AppColors {
   static Color greySub = const Color(0xff4b4b4b);
 
   static customSnackBar(String errorMessage, Size deviceSize, bool isError) {
+    //SUCCESS ICON
+    final Icon successIcon = Icon(
+      Icons.check_circle,
+      color: blueColor,
+      size: 25,
+    );
+
+//ERROR ICON
+    final Icon errorIcon = Icon(
+      Icons.error,
+      color: redColor,
+      size: 25,
+    );
+
     return SnackBar(
       behavior: SnackBarBehavior.floating,
       backgroundColor: AppColors.whiteColor,
       margin: EdgeInsets.only(bottom: deviceSize.height - 150),
       content: Container(
         child: Center(
-          child: Text(
-            errorMessage,
-            style: TextStyle(
-              color: isError ? redColor : blueColor,
-              fontWeight: FontWeight.bold,
-              fontSize: deviceSize.width * 0.04,
-            ),
+          child: Row(
+            children: [
+              isError ? errorIcon : successIcon,
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    errorMessage,
+                    style: TextStyle(
+                      color: isError ? redColor : blueColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: deviceSize.width * 0.04,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
