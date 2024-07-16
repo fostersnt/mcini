e04b671e69978b0902936534f61ab72f5dbe5ff9
Foster Amponsah Asante <fostersnt@gmail.com>
2024-07-16 08:49:31 +0000

    Added search page

--- lib/screens/home/hero_section.dart
+++ lib/screens/home/hero_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mcini/utilities/app_colors.dart';
+import 'package:mcini/screens/home/search_page.dart';
 
 class HeroSection extends StatelessWidget {
   final Size deviceSize;
@@ -40,6 +41,10 @@ class HeroSection extends StatelessWidget {
                 ),
                 onTap: () {
                   print("Search icon is clicked");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SearchPage()),
+                  );
                 },
               ),
             ],
---
+++ lib/screens/home/search_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchController = TextEditingController();
+  List<dynamic> _results = [];
+  bool _isLoading = false;
+
+  Future<void> _search() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    String query = _searchController.text;
+    if (query.isEmpty) {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
+    // Replace with your API URL
+    final response =
+        await http.get(Uri.parse('https://api.example.com/search?q=$query'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _results = json.decode(response.body);
+        _isLoading = false;
+      });
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            border: InputBorder.none,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: _search,
+          ),
+        ],
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : _results.isEmpty
+              ? Center(child: Text('No results found'))
+              : ListView.builder(
+                  itemCount: _results.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_results[index]['title']),
+                      subtitle: Text(_results[index]['description']),
+                    );
+                  },
+                ),
+    );
+  }
+}
