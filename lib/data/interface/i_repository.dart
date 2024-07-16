e7317e4c447c913402b756e055def998cac55cec
Foster Amponsah Asante <fostersnt@gmail.com>
2024-07-16 08:59:15 +0000

    Changed http to https

--- lib/data/interface/i_repository.dart
+++ lib/data/interface/i_repository.dart
@@ -1,5 +1,5 @@
 abstract class IRepository<GWOSEVO> {
-  static const String apiBaseURL = 'http://mcini.tv/api/v1';
+  static const String apiBaseURL = 'https://mcini.tv/api/v1';
 
   Future<List<GWOSEVO>> getAllData();
   Future<GWOSEVO> getSingleData();
